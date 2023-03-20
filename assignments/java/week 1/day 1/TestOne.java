f85c1823b7e654f3883f3b4d7d6cd4b07c17c821
murielfossi <mina78aaa@gmail.com>
2023-03-20 14:25:25 -0500

    assignment 1

---
+++ assignments/java/week 1/day 1/TestOne.java
@@ -0,0 +1,22 @@
+public class TestOne{
+    public static void main(String[] args){
+
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 years old");
+         System.out.println("My hometown is Burbank, CA");
+
+
+   
+   
+         String  x= "My name is Coding Dojo";
+        String  y= "I am 100 years old";
+        String  z= "My hometown is Burbank, CA";
+
+        System.out.println(x+ "\n"+ y + "\n"+ z);
+
+
+    }
+
+
+   
+}
